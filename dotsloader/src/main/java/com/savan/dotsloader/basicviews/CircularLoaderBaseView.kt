dc2f5f458c0abe98e6749fba79516129fbfb43d1
savans <savans@tridhyaintuit.com>
2023-10-18 14:23:38 +0530

    First Loader Commit

---
+++ dotsloader/src/main/java/com/savan/dotsloader/basicviews/CircularLoaderBaseView.kt
@@ -0,0 +1,50 @@
+package com.savan.dotsloader.basicviews
+
+import android.content.Context
+import android.util.AttributeSet
+import com.savan.dotsloader.contracts.CircularAbstractView
+
+/**
+ * Created by suneet on 12/29/17.
+ */
+class CircularLoaderBaseView : CircularAbstractView {
+
+    constructor(context: Context) : super(context) {
+        initCordinates()
+        initPaints()
+    }
+
+    constructor(context: Context, dotsRadius: Int, bigCircleRadius: Int, dotsColor: Int) : super(context) {
+        this.radius = dotsRadius
+        this.bigCircleRadius = bigCircleRadius
+        this.defaultColor = dotsColor
+
+        initCordinates()
+        initPaints()
+    }
+
+    constructor(context: Context, dotsRadius: Int, bigCircleRadius: Int, dotsColorsArray: IntArray) : super(context) {
+        this.radius = dotsRadius
+        this.bigCircleRadius = bigCircleRadius
+        this.dotsColorsArray = dotsColorsArray
+        this.useMultipleColors = true
+
+        initCordinates()
+        initPaints()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initAttributes(attrs)
+        initCordinates()
+        initPaints()
+        initShadowPaints()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initAttributes(attrs)
+        initCordinates()
+        initPaints()
+        initShadowPaints()
+    }
+
+}
---
+++ dotsloader/src/main/java/com/savan/dotsloader/contracts/LoaderContract.kt
@@ -0,0 +1,10 @@
+package com.savan.dotsloader.contracts
+
+import android.util.AttributeSet
+
+/**
+ * Created by suneet on 9/18/17.
+ */
+interface LoaderContract {
+    fun initAttributes(attrs: AttributeSet)
+}
